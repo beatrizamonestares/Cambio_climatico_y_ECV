0748f98e6c0ba370da0596f28eb70feb2dbc4e1a
Samuel <80346399+SamuelLozanoJuarez@users.noreply.github.com>
2021-11-24 01:43:22 +0100

    Gráfica datos meteorológicos

--- Cambio_climatico_y_ECV.R
+++ Cambio_climatico_y_ECV.R
@@ -17,6 +17,7 @@ library(dplyr)
 library(tidyverse)
 library(fs)
 library(readxl)
+library(ggplot2)
 
 # API KEY AEMET -----------------------------------------------------------
 # Si no se detecta la presencia de una API KEY de AEMET, se abrirá la página oportuna para su obtención
@@ -36,12 +37,12 @@ Provincias <- c("Alava","Albacete","Alicante","Almeria","Asturias","Avila","Bada
 
 # * Vector de estaciones --------------------------------------------------
 # Vector que contiene el identificador de cada estación provincial de AEMET. Ocupan la misma posición en este vector que su provincia correspondiente en el vector Provincias, así el identificador 9091O se corresponde con Álava, el 8175 con Albacete ... y el 9434 con Zaragoza.
-Estaciones <- c("9091O","8175","8019","6325O","1212E","2444","4452","B954","0200E","1082","2331","3469A","5960","1109","8500A","5000C","4121","5402","1387E","8096","0367","5530E","3168D","1014A","4642E","9898","5270B","2661","9771C","1505","3129","6155A","6000A","7178I","9263D","1690A","2374X","C029O","1495","9170","2867","C447A","2465","5783","2030","9981A","9381I","3260B","8416","2422","2614","9434")
+Estaciones <- c("9091O","8175","8019","6325O","1212E","2444","4452","B954","0200E","1082","2331","3469A","5960","1109","8500A","5000C","4121","5402","1387","8096","0367","5530E","3168D","1014A","4642E","9898","5270B","2661","9771C","1505","3129","6155A","6000A","7178I","9263D","1690A","2374X","C029O","1495","9170","2867","C447A","2465","5783","2030","9981A","9381I","3260B","8416","2422","2614","9434")
 
 # CARGA DE DATOS ----------------------------------------------------------
 
 # * Datos meteorológicos --------------------------------------------------
-# A continuación empleando un bucle for y la función aemet_monthly_period() del paquete climaemet vamos a importar los datos meteorológicos de cada provincia en el periodo temportal de 2010 a 2019, creando un Data Frame para cada provincia.
+# A continuación empleando un bucle for y la función aemet_monthly_period() del paquete climaemet vamos a importar los datos meteorológicos de cada provincia en el periodo temporal de 2010 a 2019, creando un Data Frame para cada provincia.
 for (i in 1:length(Provincias)){
   Nam <- paste(Provincias[i], "Meteo", sep="")
   Objeto <- aemet_monthly_period(station = Estaciones[i], start = 2010, end = 2019)
@@ -78,6 +79,8 @@ SoriaMeteo <- Reorder(SoriaMeteo)
 
 ValladolidMeteo <- bind_rows(ValladolidMeteo, aemet_monthly_clim(station = "2422", year = 2012))
 ValladolidMeteo <- Reorder(ValladolidMeteo)
+
+
 
 # * Datos de morbilidad ---------------------------------------------------
 DFMorbilidad <- data.frame()
@@ -177,3 +180,33 @@ DFMorbilidad <- relocate(DFMorbilidad,c(`Periodo`,`Sexo`),.before = `Alava`) %>%
 
 # * Datos de mortalidad provincial ----------------------------------------
 
+
+
+# ANÁLISIS DE LOS DATOS ---------------------------------------------------
+
+
+# * Datos meteorológicos --------------------------------------------------
+
+y<- left_join(x = A_CorunaMeteo, y = A_CorunaMeteo %>% 
+                group_by(Mes) %>% 
+                summarise(Av_tmax = mean(tm_max, na.rm = TRUE),Av_tmin = mean(tm_min, na.rm = TRUE),Av_qmax = mean(q_max, na.rm = TRUE),Av_qmin = mean(q_min, na.rm = TRUE),Av_psol = mean(p_sol, na.rm = TRUE))
+                , by = "Mes") %>% 
+  mutate(Anom_tmax = (`tm_max`-`Av_tmax`)**2, Periodo_Mes = paste(Periodo,Mes,sep=" "), Anom_tmin = (`tm_min`-`Av_tmin`)**2, Anom_qmax = (`q_max`-`Av_qmax`)**2, Anom_qmin = ((`q_min`-`Av_qmin`)**2), Anom_psol = ((`p_sol`-`Av_psol`)**2))
+
+ggplot(y , aes(x = Periodo_Mes)) + 
+  geom_line(aes(y = Anom_tmax, group = 1), colour = "red",) + 
+  geom_point(size = 1.5, aes(y = Anom_tmax), colour = "red") + 
+  geom_smooth(aes( y = Anom_tmax, group = 1), fill = "red", alpha = 0.25, colour = "red")+
+  geom_line(aes(y = Anom_tmin,  group = 1), colour = "blue",) + 
+  geom_point(size = 1.5, aes(y = Anom_tmin), colour = "blue") + 
+  geom_smooth(aes(y = Anom_tmin, group = 1), fill = "blue", alpha = 0.25, colour = "blue")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+ggplot(y , aes(x = Periodo_Mes)) + 
+  geom_line(aes(y = Anom_qmax, group = 1), colour = "red",) + 
+  geom_point(size = 1.5, aes(y = Anom_qmax), colour = "red") + 
+  geom_smooth(aes( y = Anom_qmax, group = 1), fill = "red", alpha = 0.25, colour = "red")+
+  geom_line(aes(y = Anom_qmin,  group = 1), colour = "blue",) + 
+  geom_point(size = 1.5, aes(y = Anom_qmin), colour = "blue") + 
+  geom_smooth(aes(y = Anom_qmin, group = 1), fill = "blue", alpha = 0.25, colour = "blue")+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
